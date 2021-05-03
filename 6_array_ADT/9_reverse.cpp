eff9efe0aee0926c6b45139a6fc5eeaa2b03ee9c
Ravi <ravigautamcs@gmail.com>
2021-05-03 14:50:06 +0530

    the code is added for the reversing of an array using the 2nd method in which we used to swap the elements of the array

--- 6_array_ADT/9_reverse.cpp
+++ 6_array_ADT/9_reverse.cpp
@@ -18,12 +18,29 @@ int * reverse1(struct Array * arr){
     return (arr->A);
 }
 
+int * reverse2(struct Array *arr){
+    int temp;
+    for(int i=0,j=arr->length-1; i<j; i++, j--){
+        temp=arr->A[i];
+        arr->A[i]=arr->A[j];
+        arr->A[j]=temp;
+    }
+    return (arr->A);
+}
+
 int main(){
     struct Array arr={{1,2,3,4,5,6,7,8,9}, 10, 9};
     int *p;
     p=reverse1(&arr);
     for(int i=0; i<arr.length; i++){
         cout<<*(p+i)<<"\t";
+    }
+    cout<<endl;
+    cout<<"now the code is reversed again so we will get the initial array as the output"<<endl;
+    int *p1;
+    p1=reverse2(&arr);
+    for(int i=0; i<arr.length; i++){
+        cout<<*(p1+i)<<"\t";
     }
     return 0;
 }
