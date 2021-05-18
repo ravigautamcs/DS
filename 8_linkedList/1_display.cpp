33fbdc546e74726fa88dca3c4dfcb86f742e1f2a
Ravi <ravigautamcs@gmail.com>
2021-05-19 01:39:47 +0530

    code added related to the operations on the linked list

--- 8_linkedList/1_display.cpp
+++ 8_linkedList/1_display.cpp
@@ -74,9 +74,70 @@ int RSum(struct Node *p){
     }
 }
 
+int Maximum(struct Node * p){
+    int max=-99999;
+    while(p!=NULL){
+        if(p->data>=max){
+            max=p->data;
+        }
+        p=p->next;
+    }
+    return max;
+}
+
+int LSearch(struct Node *p, int key){
+    while(p!=NULL){
+        if(key==p->data){
+            return 1;
+        }
+        p=p->next;
+    }
+    return 0;
+};
+
+int RLSearch(struct Node *p, int key){
+    if(p==NULL){
+        return 0;
+    }
+    else if(key==p->data){
+        return p->data;
+    }
+    return RLSearch(p->next, key);
+}
+
+void Print(int x){
+    if(x==0){
+        cout<<"Not Found";
+    }
+    else{
+        cout<<"Key found";
+    }
+}
+
+void Insert(int pos, int x){
+    Node *p, *t;
+    if(pos==0){
+        t=new Node;
+        t->data=x;
+        t->next=first;
+        first=t;
+    }
+    else if(pos>0){
+        p=first;
+        for(int i=0; i<pos-1 && p; i++){
+            p=p->next;
+        }
+        if(p){
+        t=new Node;
+        t->data=x;
+        t->next=p->next;
+        p->next=t;
+        }
+    }
+}
 
 int main(){
-    int A[]={3,5,6,10,15};
+    int A[]={3,5,16,10,15};
     //first= new Node[sizeof(struct Node)]; 
     create(A, 5);
     Display(first);
@@ -86,6 +147,15 @@ int main(){
     cout<<"using loop : "<<Length(first)<<" using the recursive call : "<<RLength(first)<<endl;
     cout<<"Calculating the sum of the elements of the node : "<<endl;
     cout<<"using loop : "<<Sum(first)<<" using the recursive call : "<<RSum(first)<<endl;
-    
+    cout<<"printing the maximum value in the linked list "<<endl;
+    cout<<Maximum(first)<<endl;
+    cout<<"Linear Search"<<endl;
+    int a=LSearch(first, 115);
+    Print(a);
+    cout<<endl;
+    cout<<"searching using the recursive call : "<<RLSearch(first, 115);
+    cout<<"printing the updated Node : "<<endl;
+    Insert(5, 130);
+    Display(first);
     return 0;
 }
