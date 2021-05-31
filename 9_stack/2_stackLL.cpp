1316bf78e8141df9348a49093933f512bfb0244b
Ravi <ravigautamcs@gmail.com>
2021-05-31 22:32:39 +0530

    submitted the pending code ffor issue 1

--- 9_stack/2_stackLL.cpp
+++ 9_stack/2_stackLL.cpp
@@ -34,12 +34,42 @@ void Display(){
 
 //complete the code for POP, peek(index), stackTop, isEmpty and isFull
 
+void isEmpty(){
+    if(top==NULL){
+        cout<<"Empty";
+    }
+    else
+        cout<<"Non Empty";
+}
+
+int peek(){
+    return top->data;
+}
+
+
+int pop(){
+    struct Node *p;
+    p=top;
+    int c=-1;
+    if(p!=NULL){
+        top=p->next;
+        c=p->data;
+        free(p);
+    }
+    return c;
+}
+
 int main(){
     
     push(10);
     push(30);
     push(20);
     Display();
+    pop();
+    Display();
+    cout<<endl;
+    cout<<peek()<<endl;
+    isEmpty();
     
     return 0;
 }
