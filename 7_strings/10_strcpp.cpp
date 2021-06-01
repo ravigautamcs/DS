e82ea77ed481a12a49095878fc2f1c7366f8d84a
Ravi <ravigautamcs@gmail.com>
2021-06-01 10:59:16 +0530

    strings in cPP

---
+++ 7_strings/10_strcpp.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string str1="ravi ";
+    string str2="gautam";
+    string str3;
+
+    string s[5];
+    for(int i=0; i<5; i++ ){
+        cin>>s[i];
+    }
+    
+    for(int i=0; i<5; i++ ){
+        cout<<s[i]<<endl;
+    }
+
+    cout<<endl;
+    //copy of the string
+    str3=str2;
+    //concatanation of the string
+    cout<<str1 + str3<<endl;
+
+
+}
