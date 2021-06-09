d1b53c69f01a57b024680dc252149517e4ff8869
Ravi <ravigautamcs@gmail.com>
2021-06-09 13:17:19 +0530

    tower of honoi problem

---
+++ 4_recurssion/10_towerofHonoi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Honoi( int n, int a, int b, int c){
+    if(n>0){
+    
+        Honoi( n-1, a,c,b);
+        printf("Disc from %d to %d.\n", a,c);
+        Honoi( n-1, b,a,c);
+    }
+    // return d;
+}
+
+int main(){
+    Honoi(5,1,2,3);
+    // cout<<"\n"<<x;
+    return 0;
+}
