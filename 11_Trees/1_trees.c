bebbd707466d9519c68beae72cfcaeb541a330cc
Ravi <ravigautamcs@gmail.com>
2021-06-10 16:30:35 +0530

    code to create tree

--- 11_Trees/1_trees.c
+++ 11_Trees/1_trees.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include<stdlib.h>
-#include "Queue.h"
+// #include "Queue.h"
 
 struct Node * root=NULL;
+
+struct Node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+};
+
+struct Queue
+{
+    int size;
+    int front, rear;
+    struct Node **Q; //first * is because the it id denoting the poiter variable and second * is because is is containing the address of the childs
+};
+
+void create(struct Queue *q, int size){
+    q->size=size;
+    q->front=q->rear=0;
+    q->Q=(struct Node **)malloc(q->size*sizeof(struct Node *));
+}
+
+void enqueue(struct Queue *q, struct Node * x){
+    if((q->rear+1)%q->size==q->front){
+        printf("Queue is full");
+    }
+    else{
+        q->rear=(q->rear+1)%q->size;
+        q->Q[q->rear]=x;
+    }
+}
+
+struct Node * dequeue(struct Queue *q){
+    struct Node* x =NULL;
+    if(q->front==q->rear)
+        printf("Queue is Empty\n");
+    else
+    {
+        q->front=(q->front+1)%q->size;
+        x=q->Q[q->front];
+    }
+    return x;
+}
+
+int isEmpty(struct Queue q)
+{
+    return q.front==q.rear;
+}
+
+
 
 void Tcreate()
 {
@@ -11,7 +60,7 @@ void Tcreate()
     struct Queue q;
     create(&q, 100);
 
-    int x;
+
     printf("Enter the value of root\n");
     scanf("%d", &x);
 
@@ -49,29 +98,26 @@ void Tcreate()
 
 void preorder(struct Node * p){
     if(p){
-        printf("%d ", p->data);
+        printf("%d\t", p->data);
         preorder(p->lchild);
         preorder(p->rchild);
     }
-    printf("\n");
 }
 
 void Inorder(struct Node *p){
     if(p){
         Inorder(p->lchild);
-        printf("%d ", p->data);
+        printf("%d\t", p->data);
         Inorder(p->rchild);
     }
-    printf("\n");
 }
 
 void Postorder(struct Node *p){
     if(p){
         Postorder(p->lchild);
-        printf("%d ", p->data);
         Postorder(p->rchild);
+        printf("%d\t", p->data);
     }
-    printf("\n");
 }
 
 int main()
@@ -79,7 +125,9 @@ int main()
     Tcreate();
 
     preorder(root);
+    printf("\n");
     Inorder(root);
+    printf("\n");
     Postorder(root);
 
     return 0;
