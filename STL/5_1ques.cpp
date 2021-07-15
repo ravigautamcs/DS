667518edfaf47ae90163d336be696c248f643436
Ravi <ravigautamcs@gmail.com>
2021-07-15 13:34:20 +0530

    basic question of maps

---
+++ STL/5_1ques.cpp
@@ -0,0 +1,25 @@
+/* 
+Given N strings, print unique strings in the lexographical order with their frequency
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> m;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        // m[s]=m[s]+1;
+        m[s]++;
+    }
+    for (auto &pr : m)
+    {
+        cout << pr.first << " " << pr.second << endl;
+    }
+    return 0;
+}
