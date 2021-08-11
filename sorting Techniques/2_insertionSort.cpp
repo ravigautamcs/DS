bffac2856c1ecf7cac3c1dc491d323fc26f1c101
Ravi <ravigautamcs@gmail.com>
2021-08-11 07:21:01 +0530

    ..

--- sorting Techniques/2_insertionSort.cpp
+++ sorting Techniques/2_insertionSort.cpp
@@ -17,8 +17,8 @@ using namespace std;
 
 int main(){
     // sorting of the array using the insertion sort
-    int n; cin>>n;
-    int A[n];
+    int n=10; 
+    int A[10];
     for(int i=0; i<n; i++){
         cin>>A[i];
     }
